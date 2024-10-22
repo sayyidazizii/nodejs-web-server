3efa15c9f4c45bea941e3016017d7241117a5422
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-10-22 22:08:45 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
+
+  const { method } = request;
+
+  if (method === "POST") {
+    let body = [];
+
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on("end", () => {
+      body = Buffer.concat(body).toString();
+      response.end(`<h1>Hai, ${body}!</h1>`);
+    });
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
